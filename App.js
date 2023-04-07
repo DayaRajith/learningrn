1ea8b0536cb8bc5dbcc9f05c3755de884cdc121b
Daya <dayarajith18@gmail.com>
2023-04-07 14:42:06 +0530

    Text

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
 
 import React from 'react';
 import { SafeAreaView, StyleSheet, TextInput, View } from 'react-native';
+import { Feather } from '@expo/vector-icons'; 
+import AsyncStorage from 'react-native';
 
 export default function App() {
   const  [text,onChangeText] = React.useState('');           
   return (
-    <View style={[styles.container,
-    {
-      flexDirection : 'column',
-    },
-  ]}>
-    <View style = {{flex:1,backgroundColor:'#3d8f7a'}}/>
+    <>
+    <AsyncStorage />
+    <View style={styles.container}>
+   
+    <View style = {{flex:2,backgroundColor:'#3d8f7a'}}/>
 <SafeAreaView>
-  {/* <View style = {{zIndex:1}}/> */}
+  {/* <View style = {{zIndex:1}}/> */} 
   <TextInput
     style= {styles.input}
     onChangeText = {onChangeText}
@@ -21,10 +22,36 @@ export default function App() {
   />
 
 </SafeAreaView>
+<Text style = {styles.text}>
+            <Text style = {styles.capitalLetter}>
+               Total Amount:{}
+            </Text>
+              </Text>
+
+              <Text style = {styles.name}>
+               Kalam<br />
+               <Text>Amount:{}</Text>
+            </Text>
+
+              <Text style = {styles.name}>
+               Jony<br />
+               <Text>Amount:{}</Text>
+            </Text>
+
+              <Text style = {styles.name}>
+              
+               Stephen<br />
+               <Text>Amount:{}</Text>
+            </Text>
 
 
-    <View style = {{flex:3,backgroundColor:'white'}}/>    
+
+
+
+    <View style = {{flex:3,backgroundColor:'white'}}/>   
+    <Feather name="plus" size={24} color="#3d8f7a"  /> 
     </View>
+    </>
   );
 }
 
@@ -32,16 +59,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
    padding:20,
+   flexDirection : 'column',
   },
   input:{
+    
     height:40,
     margin:12,
     borderWidth:2,
     padding:10,
     borderRadius:8,
+    zIndex:1,
     borderBottomColor: '#3d8f7a',
     borderTopColor: '#3d8f7a',
     borderRightColor: '#3d8f7a',
     borderLeftColor: '#3d8f7a',
   },
+  text:{
+    color:'#3d8f7a' ,
+    textAlign:"center",
+    fontWeight: 'bold',
+    marginVertical:40 ,
+   
+    },
+
+    name:{
+      fontWeight: 'bold',
+      padding:20,
+        },
 });
